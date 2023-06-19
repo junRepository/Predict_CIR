efba802a74b85472e4844c55af28d34659d5262d
junRepository <junwoonam7786@gmail.com>
2023-06-19 20:14:05 +0900

    modify code

--- app/predict.py
+++ app/predict.py
@@ -59,11 +59,11 @@ class NeuralNet(torch.nn.Module):
 def predict_model(data):
     model = NeuralNet(8,2)
 
-    loaded_scaler = joblib.load('./models/minmax_scaler_feature8.pkl' )
+    loaded_scaler = joblib.load('/models/minmax_scaler_feature8.pkl' )
     data_scaled = loaded_scaler.transform(data)
     data_tensor = torch.FloatTensor(data_scaled)
 
-    model.load_state_dict(torch.load('./models/predict_model_feature8.pth', map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load('/models/predict_model_feature8.pth', map_location=torch.device('cpu')))
     model.eval()
 
     with torch.no_grad():
